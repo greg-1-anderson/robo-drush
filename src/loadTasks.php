08487e710f47ce2f4f750650e18aa90eecdacab8
Greg Anderson <greg.1.anderson@greenknowe.org>
2016-08-15 16:33:50 -0700

    Update for Robo 1.0.0 (specifically, RC1).

--- src/loadTasks.php
+++ src/loadTasks.php
@@ -5,11 +5,23 @@ namespace Boedah\Robo\Task\Drush;
 trait loadTasks
 {
     /**
+     * Return services.
+     */
+    public static function getDrushServices()
+    {
+        return new SimpleServiceProvider(
+            [
+                'taskDrushStack' => DrushStack::class,
+            ]
+        );
+    }
+
+    /**
      * @param string $pathToDrush
      * @return DrushStack
      */
     protected function taskDrushStack($pathToDrush = 'drush')
     {
-        return new DrushStack($pathToDrush);
+        return $this->task(__FUNCTION__, $pathToDrush);
     }
 }
